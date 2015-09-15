7d138194cdcd162303cf4e4f7fed8955154a9a55
Cícero Pablo <cicero.pablos@gmail.com>
2015-09-15 00:59:09 -0300

    Implemented `floatdrop/pinkie-promise` ponyfill

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 'use strict';
 
+var Promise = require('pinkie-promise');
+
 module.exports = function(px, base, unit) {
-  if (typeof px !== 'number') {
-    throw new TypeError('`px` should be a number');
-  }
+  return new Promise(function(resolve, reject) {
+    if (typeof px !== 'number') {
+      reject(new TypeError('`px` should be a number'));
+    }
 
-  if (typeof base === 'boolean') {
-    unit = base;
-    base = null;
-  }
+    if (typeof base === 'boolean') {
+      unit = base;
+      base = null;
+    }
 
-  base = base || 16;
-  unit = unit || false;
+    base = base || 16;
+    unit = unit || false;
 
-  if (typeof base !== 'number' || base < 0) {
-    throw new TypeError('`base` should be a number greater than zero');
-  }
+    if (typeof base !== 'number' || base < 0) {
+      reject(new TypeError('`base` should be a number greater than zero'));
+    }
 
-  var value = px / base;
-  return unit ? value + 'em' : value;
+    var em = Math.round((px / base) * 1000) / 1000;
+    resolve(unit ? em + 'em' : em);
+  });
 };
